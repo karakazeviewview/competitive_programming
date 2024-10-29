f9b3678290156140dc4788d11f805dc8b4988260
mmatsuo <36731954+karakazeviewview@users.noreply.github.com>
2024-10-29 18:45:23 +0900

    Update practice_a.cpp

--- practice_a.cpp
+++ practice_a.cpp
@@ -1,133 +1,121 @@
+// やりたいこと
+// 足し算関数を作る, 数値を文字に変換する文字化関数を作る, 文字列表示関数を作る, つまりなるべく標準ライブラリを使わず3つの関数を作りたい
+// 足し算関数は引数の個数の変化に対応、int型で返り値は合計値sum
+
+// 文字化変数はvoid型で返り値無し
+
+// 文字列表示関数はvoid型で返り値なしでwriteシステムコールを使う
+// 文字列最後の'\0'でない限りカウンタ変数でループ
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
-// 以下やりたいこと
-// なるべくシステムコールで関数自作
-// 引数の個数の変化に対応する足し算関数を作る
-// 文字型化関数を作る
-// 文字列表示関数を作る
 
-int	my_strlen(char str[])
+/*
+int		my_addabc(int a, int b, int c)
 {
-	int len = 0;
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
+	int	sum; // 合計値を格納
+	sum = a + b + c;
+	return sum;
 }
+*/
 
-	//void func(char *arg_1, ...)
-int calc_add(int num_of_arg, int num1, ...)
+
+int calc_add( int num_of_arg, int num1, ...)
 {
+	// printf("n_o_a = %d\n", num_of_arg);
 	int sum = 0;
 	va_list ap;
-	//va_start(ap, arg_1);
 	va_start(ap, num1);
-	printf("sum = %d\n", sum);
-	printf("num1: %d\n", num1);
-	//printf("num1: %d\n", va_start(ap, num1));
-	//printf("arg_2: %d\n", va_arg(ap, int));
-	//printf("arg_3: %lf\n", va_arg(ap, double));
-	//printf("nun1: %d\n", va_arg(ap, int));
 	sum = sum + num1;
+	// printf("sum = %d\n", sum);
 	int i = 0;
-	num_of_arg =  num_of_arg - 1;
-	while (i < num_of_arg)
+	while (i <= num_of_arg)
 	{
+		// printf(" num%d\n", i + 1);
 		sum = sum + va_arg(ap, int);
-		//printf("num%d = num");
+		// printf("sum = %d\n", sum);
 		i++;
+		num_of_arg--;
 	}
-	printf("num2: %d\n", va_arg(ap, int));
-	//printf("num3: %d\n", va_arg(ap, int));
-	//sum = num1 + va_arg(ap, int) + va_arg(ap, int);
-	//sum = va_arg(ap, int);
-	//sum = num1;
-	//sum = (va_arg(ap, int));
-	printf("sum = %d\n", sum);
-	//sum = sum;
 	return sum;
 }
 
-
-
-void	num_to_char(int ans)
+void	my_numtochar(int num)
 {
-	int			sho;
-	int			keta;
-	int			num;
-	int			cpnum;
-	double	warukazu;
-	char		word[32];
-	int			i;
+	int keta;
+	int ketacp;
+	int warukazu;
+	int amari;
+	int i;
+	int j;
+	int k;
+	int numcp;
+	int res;
+	char chared_num[32];
 
-	keta = 0;
-	num = ans;
-	sho = num;
-	cpnum = num;
-	warukazu = 0.1;
 	i = 0;
-	while (cpnum > 0)
+	keta = 1;
+	numcp = num;
+	while (numcp / 10 > 0)
 	{
-		cpnum = cpnum / 10;
-		i++;
+		numcp = numcp / 10; // 桁数を数える
 		keta++;
-		warukazu = warukazu * 10; 
-		//printf("keta = %d\nwarukazu = %f\n", keta, warukazu);
 	}
-	cpnum = num;
-    i = 0;
-    while (keta > 0)
-    {
-	    word[i] = cpnum / warukazu + '0';
-	    write(1, &word[i], 1);
-	    cpnum = cpnum - warukazu * (word[i] - '0');
-	    warukazu = warukazu / 10;
-	    keta--;
-	    i++;
-    }
-		write (1, " ", 1);
-    return ;
-}
-
-void	print_str(char str[])
-{
-	int	i = 0;
-	while (str[i] != '\0')
+	//printf("keta = %d\n", keta);
+	ketacp = keta;
+	numcp = num;
+	i = ketacp - 1;
+	while (ketacp > 0)
 	{
-		write (1, &str[i], 2);
-		str++;
+		amari = numcp % 10;
+		//printf("amari = %d\n", amari);
+		numcp = numcp / 10;
+		//printf("numcp = %d\n", numcp);
+		//printf("chared_num[%d] = %c\n", i, amari + '0');
+		chared_num[i] = amari + '0';
+		i--;
+		ketacp--;
+	}
+	//printf("\n");
+	ketacp = keta;
+	i = 0;
+	while (keta > i)
+	{
+		write(1, &chared_num[i], 1);
 		i++;
 	}
+	//printf("\n");
+
+	//return *chared_num;
 }
 
-int main(void)
+void	my_putchar(char str[])
 {
-	int i = 0;
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int sum = 0;
-	int data = 0;
-	int ans = 0;
-	char str[32];
-	int num  ;
-
-	//ans = calc_add(num, ans);
-	printf("%d\n", ans);
-	while (i < 3)
+	int i = 0; // カウンタ変数
+	while (str[i] != '\0') // 文字列の最後でない限りループ
 	{
-		scanf("%d%d%d", &a, &b, &c);
-		//calc_add();
-		//calc_add(a, b, c);
-		//printf("num = %d\n sum = %d\n", num, sum);
-		//sum = sum + num;
-		i++;
+		write (1, &str[i], 1);
+		i = i + 1;
 	}
+}
+
+int		main(void)
+{
+	int		a;
+	int		b;
+	int		c;
+	int 	num_of_args;
+	int 	res;
+	char	str[1000];
+	char chared_num[1000];
+
+	num_of_args = scanf("%d%d%d", &a, &b, &c);
 	scanf("%s", str);
-	num_to_char(sum);
-	print_str(str);
-	printf("\n");
+	res = calc_add(num_of_args, a, b, c);
+	my_numtochar(res);
+	write(1, " ", 1);
+	my_putchar(str);
+	// chared_num = my_numtochar(num);
+	// printf("%s\n", chared_num);
 	return (0);
 }
